656a7820c27a06524805067c720c3cfb3a82150c
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-02-19 15:37:59 +0500

    Add project files.

---
+++ ConsoleApp15/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Number { get; set; }
+        public string Position { get; set; }
+        public Team Team { get; set; }
+        public void Info()
+        {
+            Console.WriteLine
+                (
+                $"Name: {Name}\n" +
+                $"Surname: {Surname}\n"+
+                $"Birthdate: {Birthdate.ToString()}\n"+
+                $"Number: {Number}\n"+
+                $"Position: {Position}\n"+
+                $"Team: {Team.Name}."
+                );
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Team Barcelona = new Team()
+            {
+                Name = "Barcelona",
+                City = "Barcelona",
+                Country = "Spain"
+            };
+            Team RealMadrid = new Team()
+            {
+                Name = "Real Madrid",
+                City = "Madrid",
+                Country = "Spain"
+            };
+            Player messi = new Player()
+            {
+                Name = "Leonel",
+                Surname = "Messi",
+                Birthdate = Convert.ToDateTime("01-01-1987"),
+                Number = 10,
+                Position = "forward",
+                Team = Barcelona
+            };
+            Player benzema = new Player()
+            {
+                Name = "Karim",
+                Surname = "Benzema",
+                Birthdate = Convert.ToDateTime("01-01-1986"),
+                Number = 9,
+                Position = "forward",
+                Team = RealMadrid
+            };
+
+            messi.Info();
+            Console.WriteLine("------------");
+            benzema.Info();
+            Console.ReadLine();
+        }
+    }
+}
